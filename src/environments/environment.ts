a6432d72f6abb46c251f3bba797dbfe36b821781
Laura <lyoussef@entrepreware.com>
2020-03-09 16:00:26 +0200

    add prefix to all requests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiHost: 'http://127.0.0.1:8081',
+  apiHost: 'http://127.0.0.1:8081/server',
   // apiHost: 'http://192.168.1.24:8081',
   production: false
 };
