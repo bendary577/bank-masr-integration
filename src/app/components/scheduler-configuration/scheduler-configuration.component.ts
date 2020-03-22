dadf20b7d98dd4916369a3ca76615d15208d9402
Laura Youssef <lyoussef@entrepreware.com>
2020-03-22 14:40:24 +0200

    1) Add business date in transfer config 2) Add month days in scheduler

--- src/app/components/scheduler-configuration/scheduler-configuration.component.ts
+++ src/app/components/scheduler-configuration/scheduler-configuration.component.ts
@@ -22,7 +22,7 @@ export class SchedulerConfigurationComponent implements OnInit {
   syncJobType: SyncJobType;
 
   hours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-  days = []
+  days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
   daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
   constructor(public dialogRef: MatDialogRef<AddVendorComponent>, private spinner: NgxSpinnerService,
@@ -42,7 +42,6 @@ export class SchedulerConfigurationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getCurrentDays()
     this.getSyncJobType()
   }
 
