4e92d8fd7cbb3ceea6dac195c5b8c16f284641ad
Ziad Muhammad <zentrepreware@gmail.com>
2021-01-24 15:04:18 +0200

    change message in case no data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  // apiHost: 'http://localhost:8081',
-  apiHost: 'http://130.211.219.76:8081',
+  apiHost: 'http://localhost:8081',
+//  apiHost: 'http://130.211.219.76:8081',
   production: false
 };
 
