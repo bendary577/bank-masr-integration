8e23a0911e0240a0d624b7a9139e8b34515da250
Laura <lyoussef@entrepreware.com>
2021-02-02 10:38:27 +0200

    view analysis in AP an CN sync

--- src/app/components/approved-invoices-infor/approved-invoices-infor.component.ts
+++ src/app/components/approved-invoices-infor/approved-invoices-infor.component.ts
@@ -203,6 +203,30 @@ export class ApprovedInvoicesInforComponent implements OnInit {
   );
   }
 
+  exportToCSV():void {
+    this.csvService.exportSalesToCSV(this.selectedJob.id).subscribe(
+      res => {
+        const blob = new Blob([res.body], { type : 'application/vnd.ms.txt' });
+        const file = new File([blob], "sales" + '.ndf', { type: 'application/vnd.ms.txt' });
+        saveAs(file);
+
+        this.snackBar.open("Export Successfully", null, {
+          duration: 2000,
+          horizontalPosition: 'center',
+          panelClass:"my-snack-bar-success"
+        });
+      },
+      err => {
+        console.error(err)
+        this.snackBar.open("Fail to export, Please try agian" , null, {
+          duration: 2000,
+          horizontalPosition: 'center',
+          panelClass:"my-snack-bar-fail"
+        });
+      }
+   );
+  }
+
   generateSingleFile():void {
     this.csvService.generateSingleFile(Constants.APPROVED_INVOICES_SYNC).subscribe(
       res => {
