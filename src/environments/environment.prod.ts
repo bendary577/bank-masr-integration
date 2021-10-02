2cac739b898e0f7538d4d7be0f8182573e7f39bd
Bassel <bassel.faisal.bs@gmail.com>
2021-10-02 04:37:55 +0200

    change env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  //apiHost: 'http://34.122.155.73:8081',
+  apiHost: 'http://34.123.229.160:8081',
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  // apiHost: 'http://localhost:8081',
+  // apiHost: 'http://130.211.219.76:8081',
+  // apiHost: 'http://34.122.155.73:8081',
+     apiHost: 'http://34.123.229.160:8081',
+
+  production: false,
+}
+
+/*
+
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
