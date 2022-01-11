10e282a58849f07e85451a4a12de005e3db152d2
Laura <lyoussef@entrepreware.com>
2022-01-11 17:05:27 +0200

    Add birthday gift settings

--- src/app/components/setting/reward-points-settings/reward-points-settings.component.ts
+++ src/app/components/setting/reward-points-settings/reward-points-settings.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MatSnackBar } from '@angular/material'
 import { NgxSpinnerService } from 'ngx-spinner'
 import { ErrorMessages } from 'src/app/models/ErrorMessages'
 import { GeneralSettings } from 'src/app/models/GeneralSettings'
+import { BirthdayGift } from 'src/app/models/loyalty/BirthdayGift'
 import { Response } from 'src/app/models/Response'
 import { AuthService } from 'src/app/services/auth/auth.service'
 import { GeneralSettingsService } from 'src/app/services/generalSettings/general-settings.service'
@@ -13,9 +15,13 @@ import { GeneralSettingsService } from 'src/app/services/generalSettings/general
   styleUrls: ['./reward-points-settings.component.scss'],
 })
 export class RewardPointsSettingsComponent implements OnInit {
+  loading = true
   generalSettings: GeneralSettings = new GeneralSettings()
+  public pointsForm: FormGroup
+  public birthdayForm: FormGroup
 
   constructor(
+    private formBuilder: FormBuilder,
     public snackBar: MatSnackBar,
     private spinner: NgxSpinnerService,
     private generalSettingsService: GeneralSettingsService,
@@ -24,15 +30,30 @@ export class RewardPointsSettingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getGeneralSettings()
+
+    this.pointsForm = this.formBuilder.group({
+      pointReward: [0, [Validators.required]],
+      pointsRedemption: [0, [Validators.required]],
+    })
+
+    this.birthdayForm = this.formBuilder.group({
+      itemId: [0, [Validators.required]],
+      offsetBefore: [0, [Validators.required]],
+      offsetAfter: [0, [Validators.required]],
+    })
   }
 
   getGeneralSettings() {
+    this.loading = true
     this.spinner.show()
     this.generalSettingsService
       .getGeneralSettings()
       .then((res) => {
         this.generalSettings = res as GeneralSettings
-
+        if (this.generalSettings.birthdayGift == null) {
+          this.generalSettings.birthdayGift = new BirthdayGift()
+        }
+        this.loading = false
         this.spinner.hide()
       })
       .catch((err) => {
@@ -50,6 +71,8 @@ export class RewardPointsSettingsComponent implements OnInit {
           horizontalPosition: 'center',
           panelClass: 'my-snack-bar-fail',
         })
+
+        this.loading = false
         this.spinner.hide()
       })
   }
---
+++ src/app/models/loyalty/BirthdayGift.ts
@@ -0,0 +1,9 @@
+export class BirthdayGift {
+    public enabled: boolean;
+    public itemId: number;
+    public offsetBefore: number;
+    public offsetAfter: number;
+    public addedPoints: number;
+
+    public constructor() { }
+}
