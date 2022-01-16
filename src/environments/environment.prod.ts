2e767b897bc617b84e14d6d7d45c16e85b17233b
p&p <136>
2022-01-16 10:54:28 +0200

    convert to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // Production Env
-  //apiHost: 'http://34.122.155.73:8081',
+  apiHost: 'http://34.122.155.73:8081',
   // Development Env
-  apiHost: 'http://35.238.14.150:8081',
+  // apiHost: 'http://35.238.14.150:8081',
   production: true
 };
