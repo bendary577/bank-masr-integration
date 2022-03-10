0e5c6b4b6d892f8645afd37f9f016c663dfea7a2
Bassel <bassel.faisal.bs@gmail.com>
2022-03-10 16:59:06 +0200

    Aggregator Configuration

--- src/app/components/aggregator-products/aggregator-products.component.ts
+++ src/app/components/aggregator-products/aggregator-products.component.ts
@@ -1,11 +1,18 @@
+
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { MatDialog, MatSnackBar } from '@angular/material';
-import { SyncJobService } from 'src/app/services/sync-job/sync-job.service';
-import { ExcelService } from 'src/app/services/excel/excel.service';
-import { CsvService } from 'src/app/services/csv/csv.service';
-import { SideNaveComponent } from '../side-nave/side-nave.component';
-import { AggregatorIntegratorService } from 'src/app/services/aggregator-integrator/aggregator-integrator.service';
+import { ErrorMessages } from 'src/app/models/ErrorMessages';
+import { GeneralSettings } from 'src/app/models/GeneralSettings';
+import { Response } from 'src/app/models/Response';
+import { AddressMapping } from 'src/app/models/talabat/address-mapping';
+import { BranchMapping } from 'src/app/models/talabat/branch-mapping';
+import { CustomerMapping } from 'src/app/models/talabat/customer-mapping';
+import { DiscountMapping } from 'src/app/models/talabat/discount-mapping';
+import { ProductMapping } from 'src/app/models/talabat/product-mapping';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { GeneralSettingsService } from 'src/app/services/generalSettings/general-settings.service';
+
 
 @Component({
   selector: 'app-aggregator-products',
@@ -14,46 +21,189 @@ import { AggregatorIntegratorService } from 'src/app/services/aggregator-integra
 })
 export class AggregatorProductsComponent implements OnInit {
 
+  newProductMapping = new ProductMapping();
+  newBranchMapping = new BranchMapping();
+  newDiscountMapping = new DiscountMapping();
+  newCustomerMapping = new CustomerMapping();
+  newAddressMapping = new AddressMapping();
 
-  loading = true;
-  products = [];
-  state = "";
+  branchMappingData = []
+  productsMappingData = []
+  customerMappingData = []
+  addressMappingData = []
+  discountMappingData = []
 
-  constructor(private spinner: NgxSpinnerService, private syncJobService: SyncJobService,
-    public snackBar: MatSnackBar, private aggregatorService: AggregatorIntegratorService, public dialog: MatDialog
-    ,private excelService: ExcelService, private sidNav: SideNaveComponent, private csvService: CsvService) { }
+  showLoading: boolean;
+  message: 'No data yet';
 
-  ngOnInit() {
-    this.getAggregatorsProducts();
+  generalSettings: GeneralSettings = new GeneralSettings();
+
+  constructor(public snackBar: MatSnackBar, private spinner: NgxSpinnerService,
+    private generalSettingsService: GeneralSettingsService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.getGeneralSettings();
   }
 
-  getAggregatorsProducts() {
+  getGeneralSettings() {
     this.spinner.show();
-    this.aggregatorService.getAggregatorProducts().toPromise().then((res: any) => {
+
+    this.generalSettingsService.getGeneralSettings().then((res) => {
+      console.log(this.generalSettings)
+
+      this.generalSettings = res as GeneralSettings;
+      this.branchMappingData = this.generalSettings.talabatConfiguration.branchMappings;
+      this.productsMappingData = this.generalSettings.talabatConfiguration.productsMappings;
+      this.customerMappingData = this.generalSettings.talabatConfiguration.customerMappings;
+      this.addressMappingData = this.generalSettings.talabatConfiguration.addressMappings;
+      this.discountMappingData = this.generalSettings.talabatConfiguration.discountMappings;
+
       this.spinner.hide();
-      this.products = res["data"]["orders"];
-      console.log(this.products)
-      this.loading = false;
-      if (res.status) {
-        this.snackBar.open(res.message, null, {
-          duration: 2000,
-          horizontalPosition: 'center',
-          panelClass:"my-snack-bar-success"
-        });
-      }
     }).catch(err => {
-      this.spinner.hide();
-      this.loading = false;
-      this.snackBar.open(err.message , null, {
+      let message = "";
+      if (err.error) {
+        message = err.error;
+      } else if (err.message) {
+        message = err.message;
+      } else {
+        message = ErrorMessages.FAILED_TO_GET_CONFIG;
+      }
+
+      this.snackBar.open(message, null, {
         duration: 3000,
         horizontalPosition: 'center',
-        panelClass:"my-snack-bar-fail"
+        panelClass: "my-snack-bar-fail"
       });
+      this.spinner.hide();
     });
   }
 
-  hasRole(reference) {
-    return this.sidNav.hasRole(reference)
+  addProductsMappingData() {
+    if (this.newProductMapping.name && this.newProductMapping.foodIcsProductId && this.newProductMapping.talabatProductId) {
+      this.productsMappingData.push(this.newProductMapping);
+      this.newProductMapping = new ProductMapping();
+
+      this.productsMappingData = [...this.productsMappingData];
+    } else {
+      this.snackBar.open('Please fill all type fields.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+    }
+  }
+
+  addBranchMappingData() {
+    if (this.newBranchMapping.name && this.newBranchMapping.foodIcsBranchId && this.newBranchMapping.talabatBranchId) {
+      this.branchMappingData.push(this.newBranchMapping);
+      this.newBranchMapping = new BranchMapping();
+
+      this.branchMappingData = [...this.branchMappingData];
+    } else {
+      this.snackBar.open('Please fill all type fields.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+    }
+  }
+
+  addDiscountMappingData() {
+    if (this.newDiscountMapping.discountId && this.newDiscountMapping.discountRate) {
+      this.discountMappingData.push(this.newDiscountMapping);
+      this.newDiscountMapping = new DiscountMapping();
+
+      this.discountMappingData = [...this.discountMappingData];
+    } else {
+      this.snackBar.open('Please fill all type fields.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+    }
+  }
+
+  addCustomerMappingData() {
+    if (this.newCustomerMapping.name && this.newCustomerMapping.foodicsId && this.newCustomerMapping.talabatId) {
+      this.customerMappingData.push(this.newCustomerMapping);
+      this.newCustomerMapping = new CustomerMapping();
+
+      this.customerMappingData = [...this.customerMappingData];
+    } else {
+      this.snackBar.open('Please fill all type fields.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+    }
+  }
+
+  addAddressMappingData() {
+    if (this.newAddressMapping.customerFoodicsId && this.newAddressMapping.foodicsId && this.newAddressMapping.talabatId) {
+      this.addressMappingData.push(this.newAddressMapping);
+      this.newAddressMapping = new AddressMapping();
+
+      this.addressMappingData = [...this.addressMappingData];
+    } else {
+      this.snackBar.open('Please fill all type fields.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+    }
+  }
+
+  onSaveClick() {
+    this.spinner.show();
+
+    try {
+
+      this.generalSettings.talabatConfiguration.productsMappings = this.productsMappingData;
+      this.generalSettings.talabatConfiguration.branchMappings = this.branchMappingData;
+      this.generalSettings.talabatConfiguration.customerMappings = this.customerMappingData;
+      this.generalSettings.talabatConfiguration.discountMappings = this.discountMappingData;
+      this.generalSettings.talabatConfiguration.addressMappings = this.addressMappingData;
+
+      console.log(this.generalSettings)
+
+      this.generalSettingsService.updateGeneralSettings(this.generalSettings).then(result => {
+        console.log(this.generalSettings)
+
+        const response = result as Response;
+        this.authService.generalSettings = this.generalSettings;
+        if (response.success) {
+          this.snackBar.open('Integration data saved successfully.', null, {
+            duration: 2000,
+            horizontalPosition: 'center',
+            panelClass: "my-snack-bar-success"
+          });
+        } else {
+          this.snackBar.open('An error has occurred.', null, {
+            duration: 2000,
+            horizontalPosition: 'center',
+            panelClass: "my-snack-bar-fail"
+          });
+        }
+        this.spinner.hide();
+      }
+      ).catch(err => {
+        this.snackBar.open('An error has occurred.', null, {
+          duration: 2000,
+          horizontalPosition: 'center',
+          panelClass: "my-snack-bar-fail"
+        });
+        this.spinner.hide();
+      });
+    } catch (e) {
+      this.snackBar.open('Failed to save simphony discount rates, Please try again.', null, {
+        duration: 2000,
+        horizontalPosition: 'center',
+        panelClass: "my-snack-bar-fail"
+      });
+
+      this.spinner.hide();
+    }
   }
 
 }
+
