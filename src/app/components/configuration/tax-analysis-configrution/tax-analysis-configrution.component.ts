ae9a85a6b774702d5d7a999b2723fa56acfd82a2
lyoussef <lyoussef@entrepreware.com>
2022-04-10 13:53:16 +0200

    WIP in config invoice taxes

--- src/app/components/configuration/tax-analysis-configrution/tax-analysis-configrution.component.ts
+++ src/app/components/configuration/tax-analysis-configrution/tax-analysis-configrution.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { InforTax } from 'src/app/models/InforTax';
 import { SyncJobType } from 'src/app/models/SyncJobType';
 
 @Component({
@@ -9,9 +11,29 @@ import { SyncJobType } from 'src/app/models/SyncJobType';
 export class TaxAnalysisConfigrutionComponent implements OnInit {
   @Input() syncJobType: SyncJobType;
 
-  constructor() { }
+  newTax = new InforTax();
+  taxes = [];
+
+  constructor(public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.taxes = this.syncJobType.configuration.inforConfiguration.taxes;
+  }
+
+
+  addTaxCode(){
+    if(this.newTax.taxCode  && this.newTax.taxPercentage){
+       this.taxes.push(this.newTax);
+       this.taxes = [...this.taxes];
+
+       this.newTax = new InforTax();
+     } else {
+       this.snackBar.open('Please fill all tax fields.', null, {
+         duration: 2000,
+         horizontalPosition: 'center',
+         panelClass:"my-snack-bar-fail"
+       });
+    }
   }
 
 }
