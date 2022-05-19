2a6ad863ffbf5d059a2008414b691e215f6cc21c
lyoussef <lyoussef@entrepreware.com>
2022-05-19 14:58:52 +0200

    add aggregator configs component

---
+++ src/app/components/aggregators-configuration/aggregators-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aggregators-configuration',
+  templateUrl: './aggregators-configuration.component.html',
+  styleUrls: ['./aggregators-configuration.component.scss']
+})
+export class AggregatorsConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
