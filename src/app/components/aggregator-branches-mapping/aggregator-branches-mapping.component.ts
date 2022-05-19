72395f45140d917506c98b7e3adb1e3f51502f27
lyoussef <lyoussef@entrepreware.com>
2022-05-19 16:47:16 +0200

    adding branch mapping component

---
+++ src/app/components/aggregator-branches-mapping/aggregator-branches-mapping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aggregator-branches-mapping',
+  templateUrl: './aggregator-branches-mapping.component.html',
+  styleUrls: ['./aggregator-branches-mapping.component.scss']
+})
+export class AggregatorBranchesMappingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
