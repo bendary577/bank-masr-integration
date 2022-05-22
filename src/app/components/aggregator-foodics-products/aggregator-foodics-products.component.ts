1f9069ab386ebe870576eb31efaddb817e5fe9cf
lyoussef <lyoussef@entrepreware.com>
2022-05-22 15:14:45 +0200

    adding foodics profucts component:

---
+++ src/app/components/aggregator-foodics-products/aggregator-foodics-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aggregator-foodics-products',
+  templateUrl: './aggregator-foodics-products.component.html',
+  styleUrls: ['./aggregator-foodics-products.component.scss']
+})
+export class AggregatorFoodicsProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
